fc74b91a2acf5c1ccd621e0e269ff1a03ee87294
Hatice Karatay <haticekaraty@gmail.com>
2021-12-19 16:47:22 -0500

    add about component

---
+++ src/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container } from "react-bootstrap"
+
+const About = () => {
+    
+        return (
+            <section className="about">
+                <div>
+                    <div>
+                        <div>
+                        <header>
+                            <h1 className="name">Hatice Karatay</h1>
+                            <h1>Software Engineer</h1>
+                            {/* <h1><a href="mailto:haticekaraty@gmail.com">haticekaraty@gmail.com</a></h1> */}
+                        </header>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    
+}
+
+export default About
