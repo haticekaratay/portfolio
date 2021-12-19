08101e1d81f1ef0ceb0220b652dd4e36e29d640f
Hatice Karatay <haticekaraty@gmail.com>
2021-12-19 16:27:39 -0500

    add navbar about skills and footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
-
+import React from 'react';
+import NavBar from "./NavBar"
+import Footer from './footer';
+import About from './About';
+import Skills from './Skills';
+import Projects from './Projects'
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <div className="content-container" >
+      <NavBar />
+      <div id="myNavbar"></div>
+      <About />
+      <Projects />
+      
+      <Skills />
+      
+      </div>
+    <Footer />
+    </>
   );
 }
 
